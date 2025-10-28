4528ef3e91c3f694671a1bd4bbe8a035e60e6fb7
Joshua Henry <henry41043@gmail.com>
2025-10-27 21:48:03 -0400

    updated ECS deployment action and cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://your-frontend-domain.com'],
+    origin: ['http://localhost:4200', 'https://btchub.bitshiftstudio.io'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization, X-ID-Token',
